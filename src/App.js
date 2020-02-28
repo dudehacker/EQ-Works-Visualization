38f82b9bd76f5fc603d8e87d90d975bc730b9431
dudehacker <xiongquansheng123@hotmail.com>
2020-02-28 14:29:45 -0500

    error handle for map api call

--- src/App.js
+++ src/App.js
@@ -1,38 +1,29 @@
-import React, {useState,useEffect} from 'react';
-import './App.css';
-import Map from "./components/Map"
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Map from "./components/Map";
+import axios from "axios";
 
 function App() {
-  const [poi, setPoi] = useState(  [] );
-  const [stats, setStats] = useState(  [] );
+  const [poi, setPoi] = useState([]);
+  const [stats, setStats] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     async function fetchData() {
-      const result = await axios(
-        process.env.REACT_APP_DATA+'/poi',
-      );
-      console.log(result.data)
-      setPoi(result.data);
+      try {
+        const result = await axios(process.env.REACT_APP_DATA + "/stats/daily");
+        setStats(result.data);
+        const result2 = await axios(process.env.REACT_APP_DATA + "/poi");
+        setPoi(result2.data);
+      } catch (e) {
+        alert(e.response.data);
+      }
     }
     fetchData();
-  },[]);
-
-  useEffect(()=>{
-    async function fetchData() {
-      const result = await axios(
-        process.env.REACT_APP_DATA+'/stats/daily',
-      );
-      console.log(result.data)
-      setStats(result.data);
-    }
-    fetchData();
-  },[]);
-
+  }, []);
 
   return (
     <div className="App">
-      <Map poi={poi} features={stats}/>
+      <Map poi={poi} features={stats} />
     </div>
   );
 }
